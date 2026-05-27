c01ea58c2ee28b8aa5ffc375e5966afe2db246e6
Ronivaldo Domingues de Andrade <ronidomingues.ard@gmail.com>
2026-05-27 05:35:33 -0300

    set MapScreen as the initial route in the navigation stack

--- App.js
+++ App.js
@@ -8,18 +8,20 @@ import Cadastro from "./src/screens/Cadastro.js";
 import Home from "./src/screens/Home.js";
 import CadastroPet from './src/screens/CadastroPet.js';
 import PerfilPet from './src/screens/PerfilPet.js';
+import MapScreen from './src/screens/MapScreen.js';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+      <Stack.Navigator initialRouteName="MapScreen" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Cadastro" component={Cadastro} />
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="CadastroPet" component={CadastroPet} />
         <Stack.Screen name="PerfilPet" component={PerfilPet} />
+        <Stack.Screen name="MapScreen" component={MapScreen} />
       </Stack.Navigator>
       <StatusBar style="auto" />
     </NavigationContainer>
